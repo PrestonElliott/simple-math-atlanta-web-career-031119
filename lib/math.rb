d485d616b28f6b54d625f5a9c9f1e1e1c6fa8443
Preston Elliott <preston.elliott24@gmail.com>
2019-03-01 23:25:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,7 +3,7 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  num1 - num2
+  return num1 - num2
 end
 
 def division(num1, num2)
@@ -15,13 +15,14 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  num1 % num2
+  return num1 % num2
 end
 
 def square_root(num)
-  Math.sqrt(num)
+  return Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
+  num1 + ((num2 * num3) / num4)
   #Hint:  __ + (( __ * __ ) / __ )
 end
